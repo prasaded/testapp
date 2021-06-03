3f80dd18213f33e171cc9e2a399d6db50d79c09b
prasaded <padmaprasadsr@gmail.com>
2021-06-03 18:55:02 +0530

    1

--- Webapp/scripts/after_install.sh
+++ Webapp/scripts/after_install.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 yum update -y
 yum install httpd -y
-echo '<h1> Welcome Pacchu </h1>' > /var/www/html/index.html
+echo '<h1> Welcome S </h1>' > /var/www/html/index.html
 chkconfig httpd on
 sudo systemctl restart httpd
