f8fb2a5f6a38853b286d1c81e7cd9e5b072c3da0
prasaded <padmaprasadsr@gmail.com>
2021-06-03 18:02:51 +0530

    index changed

--- Webapp/scripts/after_install.sh
+++ Webapp/scripts/after_install.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 yum update -y
 yum install httpd -y
-echo '<h1> Welcome P </h1>' > /var/www/html/index.html
+echo '<h1> Welcome Pacchu </h1>' > /var/www/html/index.html
 chkconfig httpd on
 service httpd start
